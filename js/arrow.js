4db696c8dfe017504befa857b75a0a76e391af47
Chen Wei Ni <bcg2135@gmail.com>
2020-01-07 23:20:59 +0800

    modfied

---
+++ js/arrow.js
@@ -0,0 +1,8 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+	$('li.dropdown a').on('hover',function() {
+		$(this)
+		.find('[data-fa-i2svg]')
+		.toggleClass('fas fa-angle-down')
+        .toggleClass('fas fa-angle-up');
+	});
+});
---
+++ js/images.js
@@ -0,0 +1,8 @@
+﻿
+$('document').ready(function() {
+	$('#mywork img').hover(function() {
+		$(this).animate({opacity: 0},'fast');
+	}, function() {
+		$(this).animate({opacity: 0.7}, 'fast');
+	});
+});
